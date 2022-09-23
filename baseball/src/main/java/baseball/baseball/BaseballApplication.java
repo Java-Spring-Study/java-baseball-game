907935cc15df9d378648b06f0bc8074100bfebfd
amaran-th <songsy405@gmail.com>
2022-09-23 17:58:33 +0900

    feat : 프로젝트 구현

--- baseball/src/main/java/baseball/baseball/BaseballApplication.java
+++ baseball/src/main/java/baseball/baseball/BaseballApplication.java
@@ -1,13 +1,24 @@
 package baseball.baseball;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import baseball.tool.BaseballTool;
 
-@SpringBootApplication
 public class BaseballApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(BaseballApplication.class, args);
+		try{
+			while(true){
+				BaseballTool tool=new BaseballTool();
+				tool.makeRamdomNumbers();	//랜덤한 숫자를 생성한다.
+
+				while(!tool.checkStrike()){	//확인한다.
+					tool.setInputNumber();	//input을 받는다.
+					tool.calcResult();		//게임 결과를 계산한다.
+					tool.printResult();		//게임 결과를 출력한다.
+				}
+				if(tool.getSignalCode()==2) break;
+			}
+		}catch(IllegalArgumentException e){
+			System.err.println("input error");
+		}
 	}
 
 }
