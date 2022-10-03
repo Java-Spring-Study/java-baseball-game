7d4d41312fef6615832a0671a5e4b6d8a5fe3c3f
Kim taegeun <oryukdo3@pusan.ac.kr>
2022-10-03 14:38:14 +0900

    feat: Class Player .. inputNum() 구현

--- java_baseball_game/src/main/java/com/taegeun/Main.java
+++ java_baseball_game/src/main/java/com/taegeun/Main.java
@@ -3,5 +3,6 @@ package com.taegeun;
 public class Main {
     public static void main(String[] args){
         GameManager gameManager = new GameManager();
+        gameManager.start();
     }
 }
--- java_baseball_game/src/main/java/com/taegeun/Player.java
+++ java_baseball_game/src/main/java/com/taegeun/Player.java
@@ -1,4 +1,18 @@
 package com.taegeun;
 
+import java.util.Scanner;
+
 public class Player {
+    int myNum;
+    public int inputNum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요 : ");
+        myNum = sc.nextInt();
+
+        if (Integer.toString(myNum).length() != 3){
+            throw new IllegalArgumentException("3자리수가 아닙니다.");
+        }
+
+        return myNum;
+    }
 }
