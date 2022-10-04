c522bd5cd0c9f72b189784524dfdc9507b213872
llddang <bodyness@naver.com>
2022-10-04 14:41:21 +0900

    feat: 야구게임 개발

---
+++ baseball/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Baseball baseball = new Baseball();
+        baseball.gameInit();
+        baseball.gameStart();
+    }
+}
