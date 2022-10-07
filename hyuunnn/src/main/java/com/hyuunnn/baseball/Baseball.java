4e69a6abf63017db4f771290a7e7caad48fee651
Hyun Yi <hy00un__@naver.com>
2022-10-07 12:07:49 +0900

    chore: 클래스 내부에서만 사용하는 메서드들을 private으로 변경

--- hyuunnn/src/main/java/com/hyuunnn/baseball/Baseball.java
+++ hyuunnn/src/main/java/com/hyuunnn/baseball/Baseball.java
@@ -8,7 +8,7 @@ public class Baseball {
   final private int GAME_END = 2;
   final private int BALL_LENGTH = 3;
 
-  public String randomize() {
+  private String randomize() {
     final Random r = new Random();
     final Set<Integer> numberList = new LinkedHashSet<>();
 
@@ -19,19 +19,19 @@ public class Baseball {
     return numberList.toString().replaceAll("[^0-9]", "");
   }
 
-  public boolean checkStrike(final char randomNumberAt, final char inputNumberAt) {
+  private boolean checkStrike(final char randomNumberAt, final char inputNumberAt) {
     return randomNumberAt == inputNumberAt;
   }
 
-  public boolean checkBall(final String inputNumber, final char randomNumberAt) {
+  private boolean checkBall(final String inputNumber, final char randomNumberAt) {
     return inputNumber.contains(String.valueOf(randomNumberAt));
   }
 
-  public boolean checkThreeStrike(final int strike) {
+  private boolean checkThreeStrike(final int strike) {
     return strike == BALL_LENGTH;
   }
 
-  public void printBaseballResult(final int strike, final int ball) {
+  private void printBaseballResult(final int strike, final int ball) {
     if (strike == 0 && ball == 0) {
       System.out.println("낫싱");
     } else if (strike == 0 && ball > 0) {
@@ -43,7 +43,7 @@ public class Baseball {
     }
   }
 
-  public boolean checkBaseball(final String inputNumber, final String randomNumber) {
+  private boolean checkBaseball(final String inputNumber, final String randomNumber) {
     int strike = 0;
     int ball = 0;
 
@@ -62,29 +62,29 @@ public class Baseball {
     return checkThreeStrike(strike);
   }
 
-  public String inputString(final Scanner scanner) {
+  private String inputString(final Scanner scanner) {
     System.out.print("숫자를 입력해주세요 : ");
     return scanner.nextLine();
   }
 
-  public void validateLength(final String inputNumber) {
+  private void validateLength(final String inputNumber) {
     if (inputNumber.length() != BALL_LENGTH) {
       throw new IllegalStateException("입력 값의 길이가 3이 아닙니다.");
     }
   }
 
-  public void validateNumberRange(final String inputNumber) {
+  private void validateNumberRange(final String inputNumber) {
     if (!inputNumber.matches("^[0-9]*$")) {
       throw new IllegalStateException("입력 값이 숫자가 아닙니다.");
     }
   }
 
-  public void validateInputNumber(final String inputNumber) {
+  private void validateInputNumber(final String inputNumber) {
     validateLength(inputNumber);
     validateNumberRange(inputNumber);
   }
 
-  public void checkRestart(final Scanner scanner) {
+  private void checkRestart(final Scanner scanner) {
     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     final int number = scanner.nextInt();
@@ -98,7 +98,7 @@ public class Baseball {
     }
   }
 
-  public void startBaseball(final String randomNumber) {
+  private void startBaseball(final String randomNumber) {
     final Scanner scanner = new Scanner(System.in);
     while (true) {
       final String inputNumber = inputString(scanner);
