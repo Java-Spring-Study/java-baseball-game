8d56d3099bb4d499b7a9db98fedf4844c790c0df
Hyun Yi <hy00un__@naver.com>
2022-11-05 00:22:52 +0900

    fix: Scanner를 한 번 할당하는 방법으로 변경

--- hyuunnn/src/main/java/com/hyuunnn/baseball/Baseball.java
+++ hyuunnn/src/main/java/com/hyuunnn/baseball/Baseball.java
@@ -1,16 +1,21 @@
 package com.hyuunnn.baseball;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.Random;
+
 
 public class Baseball {
 
   final private int GAME_RESTART = 1;
   final private int GAME_END = 2;
   final private int BALL_LENGTH = 3;
+  final private Scanner scanner = new Scanner(System.in);
 
   private String randomize() {
-    final Random r = new Random();
-    final Set<Integer> numberList = new LinkedHashSet<>();
+    Random r = new Random();
+    Set<Integer> numberList = new LinkedHashSet<>();
 
     while (numberList.size() < BALL_LENGTH) {
       numberList.add(r.nextInt(9) + 1);
@@ -19,19 +24,19 @@ public class Baseball {
     return numberList.toString().replaceAll("[^0-9]", "");
   }
 
-  private boolean checkStrike(final char randomNumberAt, final char inputNumberAt) {
+  private boolean checkStrike(char randomNumberAt, char inputNumberAt) {
     return randomNumberAt == inputNumberAt;
   }
 
-  private boolean checkBall(final String inputNumber, final char randomNumberAt) {
+  private boolean checkBall(String inputNumber, char randomNumberAt) {
     return inputNumber.contains(String.valueOf(randomNumberAt));
   }
 
-  private boolean checkThreeStrike(final int strike) {
+  private boolean checkThreeStrike(int strike) {
     return strike == BALL_LENGTH;
   }
 
-  private void printBaseballResult(final int strike, final int ball) {
+  private void printBaseballResult(int strike, int ball) {
     if (strike == 0 && ball == 0) {
       System.out.println("낫싱");
     } else if (strike == 0 && ball > 0) {
@@ -43,13 +48,13 @@ public class Baseball {
     }
   }
 
-  private boolean checkBaseball(final String inputNumber, final String randomNumber) {
+  private boolean checkBaseball(String inputNumber, String randomNumber) {
     int strike = 0;
     int ball = 0;
 
     for (int i = 0; i < randomNumber.length(); i++) {
-      final char randomNumberAt = randomNumber.charAt(i);
-      final char inputNumberAt = inputNumber.charAt(i);
+      char randomNumberAt = randomNumber.charAt(i);
+      char inputNumberAt = inputNumber.charAt(i);
 
       if (checkStrike(randomNumberAt, inputNumberAt)) {
         strike++;
@@ -62,54 +67,53 @@ public class Baseball {
     return checkThreeStrike(strike);
   }
 
-  private String inputStringNumber(final Scanner scanner) {
+  private String inputStringNumber() {
     System.out.print("숫자를 입력해주세요 : ");
     return scanner.nextLine();
   }
 
-  private void validateLength(final String inputNumber) {
+  private void validateLength(String inputNumber) {
     if (inputNumber.length() != BALL_LENGTH) {
       throw new IllegalStateException("입력 값의 길이가 3이 아닙니다.");
     }
   }
 
-  private void validateNumberRange(final String inputNumber) {
+  private void validateNumberRange(String inputNumber) {
     if (!inputNumber.matches("^[0-9]*$")) {
       throw new IllegalStateException("입력 값이 숫자가 아닙니다.");
     }
   }
 
-  private void validateInputNumber(final String inputNumber) {
+  private void validateInputNumber(String inputNumber) {
     validateLength(inputNumber);
     validateNumberRange(inputNumber);
   }
 
-  private void checkRestart(final Scanner scanner) {
+  private void checkRestart() {
     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    final int number = scanner.nextInt();
+    int number = Integer.parseInt(scanner.nextLine());
 
     if (number == GAME_RESTART) {
       run();
     } else if (number == GAME_END) {
+      scanner.close();
       System.exit(0);
     } else {
       throw new IllegalStateException("입력 값이 1 또는 2가 아닙니다.");
     }
   }
 
-  private void start(final String randomNumber) {
-    final Scanner scanner = new Scanner(System.in);
+  private void start(String randomNumber) {
     while (true) {
-      final String inputNumber = inputStringNumber(scanner);
+      String inputNumber = inputStringNumber();
       validateInputNumber(inputNumber);
 
       if (checkBaseball(inputNumber, randomNumber)) {
         break;
       }
     }
-    checkRestart(scanner);
-    scanner.close();
+    checkRestart();
   }
 
   public void run() {
